a2a0d838f8581a78d71d5abe73311da38302cf97
LauchlynDowns <LAUCHLYN101@gmail.com>
2023-01-28 00:06:47 +0000

    styled dashboard page

--- WearTracker/resources/views/dashboard.blade.php
+++ WearTracker/resources/views/dashboard.blade.php
@@ -1,8 +1,29 @@
 <x-app-layout>
 <body class="mainpagebody">
     <div class="garage">
-        
+        <div class="parent">
+            <div class="garage-item-top-image">
+                <div class="garage-item-text">Propain Spindrift</div>
+            </div>
+            <div class="mileageinfo"><div class="mileageinfo-miles">3000mi</div><div class="mileageinfo-miles">Owned since march 2021</div></div>
+       <div class="component-holder">
+        <div class="component">w</div>
+            <div class="component">w</div>
+             <div class="component">w</div>
+          
+             
+        </div>
+        <div class="parent-bottom-buttons">
+       <div class="parent-bottom-buttons-button">
+        Button
+       </div>
+       <div class="parent-bottom-buttons-button">
+        Button
+       </div>
+        </div>
     </div>
+
+
        <div class="bottom-area-container">
         <div class="nav-item">
             <div class="nav-icon">🚲</div>
